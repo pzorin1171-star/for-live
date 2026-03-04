71e5f47caa7bd3cbb8e5881f657f4edc69cf6297
pzorin1171-star <pzorin1171@gmail.com>
2026-03-04 20:17:58 +0300

    Create formatters.ts

---
+++ src/utils/formatters.ts
@@ -0,0 +1,8 @@
+export function formatCurrency(value: number): string {
+  return new Intl.NumberFormat('ru-RU', {
+    style: 'currency',
+    currency: 'RUB',
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  }).format(value);
+}
